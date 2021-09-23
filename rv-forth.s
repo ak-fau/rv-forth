40f6f8ee40f0677ac3f33dc928cade37a5c742a6
Anton Kuzmin <anton.kuzmin@cs.fau.de>
2021-09-24 00:59:26 +0200

    Implement getchar()/putchar() and a simple lool to try them out

--- rv-forth.s
+++ rv-forth.s
@@ -10,5 +10,13 @@ _start:
         .text
         .global _cold_start
 _cold_start:
+        la sp, _end_of_ram - 16
+
+1:      call _getchar
+        li t0, 'q'
+        beq a0, t0, 1f
+        call _putchar
+        j 1b
+1:
         li a0, 0
         tail _exit
--- syscall.s
+++ syscall.s
@@ -3,10 +3,12 @@
  */
 
         .equ SYSCALL_exit,  93
+        .equ SYSCALL_read,  63
+        .equ SYSCALL_write, 64
 
         .section .init
         .global _init
-_init:
+_init:  wfi
         j _init
 
         .global _exit
@@ -18,3 +20,38 @@ _exit:
 	mv a5, zero
 	li a7, SYSCALL_exit
 	ecall
+1:
+        wfi
+        j 1b
+
+        .global _putchar
+_putchar:
+        addi sp, sp, -4
+        sb a0, 0(sp)
+        mv a1, sp
+        mv a6, a0
+        li a0, 1
+        li a2, 1
+        li a7, SYSCALL_write
+        ecall
+        addi sp, sp, 4
+        blez a0, 1f
+        mv a0, a6
+        ret
+1:      li a0, -1
+        ret
+
+        .global _getchar
+_getchar:
+        addi sp, sp, -4
+        mv a1, sp
+        li a0, 0
+        li a2, 1
+        li a7, SYSCALL_read
+        ecall
+        addi t0, a0, -1
+        lbu a0, 0(sp)
+        beqz t0, 1f
+        li a0, -1
+1:      addi sp, sp, 4
+        ret
