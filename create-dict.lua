46a80449c77a9cecd258cac41a712fd07f3a7d23
Anton Kuzmin <anton.kuzmin@cs.fau.de>
2024-03-17 00:50:37 +0100

    Store names with length greater than 3 in a separate section

--- create-dict.lua
+++ create-dict.lua
@@ -1,17 +1,46 @@
 local sub = string.sub
+local fmt = string.format
+local gsub = string.gsub
+local bit = require "bit32"
+
+local function normalize_name(s)
+   local subst = {
+      ['-'] = "minus",
+      ['+'] = "plus",
+      ['/'] = "div",
+      ['*'] = "mul",
+   }
+   if #s == 1 then
+      return subst[s] or s
+   else
+      return gsub(s, "-+/*", "_")
+   end
+end
 
 local function create_dict(list)
    local p_name = "n_null"
+   local s_offset = 0
 
    print("    .section .rodata")
    print("    .align 2")
    print(p_name .. ": .word 0, 0")
 
    for _,w in ipairs(list) do
+      local name = "n_" .. normalize_name(w)
+
       if #w > 3 then
-         error "Name length >3 is not supported [yet]"
+         local s_name = "s_" .. normalize_name(w)
+         print("    .section .strings")
+         print("    .align 2")
+         print(s_name .. ": .ascii \"" .. w .. "\"")
+         print("    .section .rodata")
+         print(name .. ":")
+         local nf = bit.lshift(#w, 24)
+         nf = bit.bor(nf, s_offset)
+         s_offset = s_offset + #w
+         s_offset = 4 * math.ceil(s_offset / 4)
+         print("    .word", fmt("0x%08x", nf))
       else
-         local name = "n_" .. w
          local bytes = ""
          for i=1,#w do
             if bytes ~= "" then bytes = bytes .. ", " end
@@ -22,10 +51,12 @@ local function create_dict(list)
          end
          print(name..": .byte", bytes)
          print("    .byte", #w)
-         print("    .word", p_name)
-         p_name = name
       end
+
+      print("    .word", p_name)
+      p_name = name
    end
+   print("")
    print(".global _dict")
    print(".equ _dict, " .. p_name)
 end
--- test-dict.lua
+++ test-dict.lua
@@ -1,5 +1,8 @@
 require "create-dict" {
    "a", "b", "c",
    "ab", "cd", "ef",
+   "abcd", "1234", "1235", "12345",
+   "-",
+   "long-name",
    "a12", "b34", "c56",
 }
